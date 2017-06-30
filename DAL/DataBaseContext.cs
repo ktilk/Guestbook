69060eddf55462c37a03e6735cdeecd41f068ad0
Kaspar Tilk <kaspar.tilk@itcollege.ee>
2017-06-30 10:41:39 +0300

    Add project files.

---
+++ DAL/DataBaseContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain;
+
+namespace DAL
+{
+    public class DataBaseContext : DbContext
+    {
+        public DataBaseContext() : base("name=DataBaseConnectionStr")
+        {
+            //Database.SetInitializer(new MigrateDatabaseToLatestVersion<DataBaseContext,Migrations.Configuration>());
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<DataBaseContext>());
+        }
+
+        public DbSet<Inscription> Inscriptions { get; set; }
+    }
+}
---
+++ Domain/Inscription.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Inscription
+    {
+        public int Id { get; set; }
+        public string WriterName { get; set; }
+        public string Content { get; set; }
+        public DateTime DateAdded { get; set; }
+        public DateTime? DateModified { get; set; }
+        public DateTime? DateDeleted { get; set; }
+    }
+}
